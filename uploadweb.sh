d8f113671a3babe4a58c58065ef8005b244821ce
axis3x3 <axis3x3@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2002-05-15 00:55:54 +0000

    Altered contributors info.
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@52 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

--- uploadweb.sh
+++ uploadweb.sh
@@ -1,2 +1,2 @@
 # This is my personal script for uploading the web pages
-scp COPYING TODO *.html *.gif axis3x3@shell.sourceforge.net:/home/groups/f/fr/freeguide-tv/htdocs/
+scp $@ axis3x3@shell.sourceforge.net:/home/groups/f/fr/freeguide-tv/htdocs/
