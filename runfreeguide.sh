2eed4693fd950a96c517131fe28e8be97f655a47
axis3x3 <axis3x3@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2002-06-13 07:44:52 +0000

    Major re-org and new installers
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@69 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

--- runfreeguide.sh
+++
@@ -1,1 +0,0 @@
-java -jar $HOME/freeguide-tv/gui-java/FreeGuide.jar
--- uploadweb.sh
+++
@@ -1,2 +0,0 @@
-# This is my personal script for uploading the web pages
-scp $@ axis3x3@shell.sourceforge.net:/home/groups/f/fr/freeguide-tv/htdocs/
