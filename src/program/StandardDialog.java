a5c078cc32e379b43f0623bea66def4e267829aa
axis3x3 <axis3x3@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2002-12-02 23:18:24 +0000

    Added ability to change fonts, using code from the JCommon library. Also german grabbing.
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@135 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

---
+++ src/program/StandardDialog.java
@@ -0,0 +1,136 @@
+/* =======================================================
+ * JCommon : a free general purpose class library for Java
+ * =======================================================
+ *
+ * Project Info:  http://www.object-refinery.com/jcommon/index.html
+ * Project Lead:  David Gilbert (david.gilbert@object-refinery.com);
+ *
+ * (C) Copyright 2000-2002, by Simba Management Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -------------------
+ * StandardDialog.java
+ * -------------------
+ * (C) Copyright 2000-2002, by Simba Management Limited.
+ *
+ * Original Author:  David Gilbert (for Simba Management Limited);
+ * Contributor(s):   -;
+ *
+ * $Id$
+ *
+ * Changes (from 26-Oct-2001)
+ * --------------------------
+ * 26-Oct-2001 : Changed package to com.jrefinery.ui.*;
+ *
+ */
+
+import java.awt.Frame;
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.BorderFactory;
+
+/**
+ * The base class for standard dialogs.
+ *
+ * @author DG
+ */
+public class StandardDialog extends JDialog implements ActionListener {
+
+    /** Flag that indicates whether or not the dialog was cancelled. */
+    private boolean cancelled;
+
+    /**
+     * Standard constructor - builds a dialog...
+     *
+     * @param owner  the owner.
+     * @param title  the title.
+     * @param modal  modal?
+     */
+    public StandardDialog(Frame owner, String title, boolean modal) {
+        super(owner, title, modal);
+        this.cancelled = false;
+    }
+
+    /**
+     * Standard constructor - builds a dialog...
+     *
+     * @param owner  the owner.
+     * @param title  the title.
+     * @param modal  modal?
+     */
+    public StandardDialog(Dialog owner, String title, boolean modal) {
+        super(owner, title, modal);
+        this.cancelled = false;
+    }
+
+    /**
+     * Returns a flag that indicates whether or not the dialog has been cancelled.
+     *
+     * @return boolean.
+     */
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    /**
+     * Handles clicks on the standard buttons.
+     *
+     * @param event  the event.
+     */
+    public void actionPerformed(ActionEvent event) {
+        String command = event.getActionCommand();
+        if (command.equals("helpButton")) {
+            // display help information
+        }
+        else if (command.equals("okButton")) {
+            this.cancelled = false;
+            hide();
+        }
+        else if (command.equals("cancelButton")) {
+            this.cancelled = true;
+            hide();
+        }
+    }
+
+    /**
+     * Builds and returns the user interface for the dialog.  This method is shared among the
+     * constructors.
+     *
+     * @return the button panel.
+     */
+    protected JPanel createButtonPanel() {
+
+        L1R2ButtonPanel buttons = new L1R2ButtonPanel("Help", "OK", "Cancel");
+
+        JButton helpButton = buttons.getLeftButton();
+        helpButton.setActionCommand("helpButton");
+        helpButton.addActionListener(this);
+
+        JButton okButton = buttons.getRightButton1();
+        okButton.setActionCommand("okButton");
+        okButton.addActionListener(this);
+
+        JButton cancelButton = buttons.getRightButton2();
+        cancelButton.setActionCommand("cancelButton");
+        cancelButton.addActionListener(this);
+
+        buttons.setBorder(BorderFactory.createEmptyBorder(4, 0, 0, 0));
+        return buttons;
+    }
+
+}
