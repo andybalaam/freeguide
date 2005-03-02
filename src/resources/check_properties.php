3851caffffd23ccea0986f572acf74e471dfa453
cweiske <cweiske@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2005-03-02 18:48:07 +0000

    There was a bug in the check_properties, and MessagesBundle_en_US.properties is ignored now
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@496 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

--- src/resources/check_properties.php
+++ src/resources/check_properties.php
@@ -7,8 +7,11 @@
 */
 
 $arCheck = array(
-    'MessagesBundle', 
+    'MessagesBundle',
     'PrivacyBundle'
+);
+$arIgnore = array(
+    'MessagesBundle_en_US.properties'
 );
 
 
@@ -23,7 +26,7 @@ function get_properties_strings($strFile)
     foreach ($arContent as $strLine) {
         $nPos = strpos($strLine, '=');
         if ($nPos !== false) {
-            $arStrings[] = substr($strLine, 0, $nPos);
+            $arStrings[] = trim(substr($strLine, 0, $nPos));
         }
     }
     return $arStrings;
@@ -40,31 +43,32 @@ foreach ($arCheck as $strBase) {
         echo ' Original file ' . $strOriginal . ' doesn\'t exist' . "\r\n";
         continue;
     }
-    
+
     $arOriginal = get_properties_strings($strOriginal);
-    
+
     //loop through all translated files
     $arTranslated   = array();
     $hdlDir = dir('.');
     while (false !== ($strFile = $hdlDir->read())) {
-        if (preg_match('/' . $strBase . '[a-zA-Z_]+\\.properties/', $strFile)) {
+        if (preg_match('/' . $strBase . '[a-zA-Z_]+\\.properties/', $strFile)
+            && !in_array($strFile, $arIgnore)) {
             $arTranslated[$strFile] = get_properties_strings($strFile);
-            
+
             $arDiff = array_diff($arOriginal, $arTranslated[$strFile]);
             if (count($arDiff) > 0) {
                 echo ' ' . $strFile . ' misses the following strings:' . "\r\n";
                 foreach( $arDiff as $strString) {
                     echo '  ' . $strString . "\r\n";
                 }
-                
+
             }//diffed result is there
             else {
                 echo ' ' . $strFile . ' ok.' . "\r\n";
             }
         }//filename matches
     }//while all files
-    
-    
+
+
 }//foreach $arCheck
 
 ?>
