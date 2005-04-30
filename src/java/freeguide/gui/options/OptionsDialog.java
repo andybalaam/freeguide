38da9542ce77b1bdbbad8e0b41253ed1038a43f3
axis3x3 <axis3x3@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2005-04-30 11:01:34 +0000

    Changed size to fit on a 600 by 400 screen.
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@763 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

--- src/java/freeguide/gui/options/OptionsDialog.java
+++ src/java/freeguide/gui/options/OptionsDialog.java
@@ -146,7 +146,7 @@ public class OptionsDialog extends FGDialog implements TreeSelectionListener,
         getRootPane(  ).setDefaultButton( okButton );
 
         pack(  );
-        setSize( new Dimension( 600, 400 ) );
+        setSize( new Dimension( 500, 350 ) );
     }
 
     /**
