97776078a81b0b369a83540c6d64c5e103c0b1aa
alex73 <alex73@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2005-07-14 15:48:26 +0000

    remove wrongs changes
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@985 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

--- src/java/freeguide/gui/wizard/WizardFrame.java
+++ src/java/freeguide/gui/wizard/WizardFrame.java
@@ -71,7 +71,7 @@ public class WizardFrame extends JWaitFrame
             Image icon =
                 ( new ImageIcon( 
                     LanguageHelper.loadResourceAsByteArray( 
-                        "freeguide/images/logo-16x16.png" ), "icon" ) ).getImage(  );
+                        "images/logo-16x16.png" ), "icon" ) ).getImage(  );
             setIconImage( icon );
         }
         catch( IOException ex )
