af090754c7ef8f962778a7d64057e27236d6ed9c
alex73 <alex73@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2005-07-28 11:57:43 +0000

    fix for automatic grabbing filenames
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@1022 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

--- src/java/freeguide/commandline/GrabForWeb.java
+++ src/java/freeguide/commandline/GrabForWeb.java
@@ -2,13 +2,17 @@ package freeguide.commandline;
 
 import freeguide.FreeGuide;
 
+import freeguide.lib.fgspecific.Application;
 import freeguide.lib.fgspecific.PluginsManager;
 import freeguide.lib.fgspecific.data.TVChannel;
 import freeguide.lib.fgspecific.data.TVData;
 import freeguide.lib.fgspecific.data.TVIteratorChannels;
 
+import freeguide.lib.general.LanguageHelper;
+
 import freeguide.lib.impexp.XMLTVExport;
 
+import freeguide.plugins.IApplication;
 import freeguide.plugins.ILogger;
 import freeguide.plugins.IModuleGrabber;
 import freeguide.plugins.IProgress;
@@ -19,6 +23,8 @@ import java.io.File;
 import java.io.IOException;
 
 import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.logging.Logger;
 
 /**
@@ -30,6 +36,9 @@ import java.util.logging.Logger;
 public class GrabForWeb
 {
 
+    protected static String path = "";
+    protected static final Map CHARCONV = new TreeMap(  );
+
     /**
      * DOCUMENT_ME!
      *
@@ -39,7 +48,18 @@ public class GrabForWeb
      */
     public static void main( final String[] args ) throws Exception
     {
+        LanguageHelper.loadProperties( 
+            GrabForWeb.class.getPackage(  ).getName(  ).replace( '.', '/' )
+            + "/filenames.replaces.properties", CHARCONV );
+
         FreeGuide.log = Logger.getLogger( "org.freeguide-tv" );
+
+        PluginsManager.loadModules(  );
+
+        Application.setInstance( 
+            (IApplication)PluginsManager.getApplicationModuleInfo(  )
+                                        .getInstance(  ) );
+
         FreeGuide.setLocale( Locale.ENGLISH );
 
         final ExportPalmAtv palmExp = new ExportPalmAtv(  );
@@ -47,10 +67,18 @@ public class GrabForWeb
         for( int i = 0; i < args.length; i++ )
         {
 
+            if( args[i].startsWith( "--path=" ) )
+            {
+                path = args[i].substring( "--path=".length(  ) ) + "/";
+
+                continue;
+            }
+
             final String siteName = args[i];
 
             IModuleGrabber grabber =
-                (IModuleGrabber)PluginsManager.getModuleByID( siteName );
+                (IModuleGrabber)PluginsManager.getModuleByID( 
+                    "grabber-" + siteName );
 
             if( grabber == null )
             {
@@ -74,8 +102,9 @@ public class GrabForWeb
                             }
                         } );
                     new XMLTVExport(  ).export( 
-                        new File( siteName + ".xml" ), result );
-                    palmExp.exportBatch( result, siteName );
+                        new File( path + siteName + ".xml" ), result );
+                    palmExp.exportBatch( 
+                        result, siteName, new File( path + siteName + ".pdb" ) );
                     new File( siteName ).mkdirs(  );
                     result.iterate( 
                         new TVIteratorChannels(  )
@@ -90,12 +119,21 @@ public class GrabForWeb
                                     TVChannel tc =
                                         oneData.get( channel.getID(  ) );
                                     tc.mergeFrom( channel );
+
+                                    File outFile =
+                                        new File( 
+                                            path + siteName + "/"
+                                            + getFNbyCN( 
+                                                channel.getDisplayName(  ) )
+                                            + ".pdb" );
+                                    outFile.getParentFile(  ).mkdirs(  );
                                     System.out.println( 
-                                        "out " + channel.getID(  ) );
+                                        "out " + outFile.getPath(  ) );
                                     palmExp.exportBatch( 
                                         oneData,
                                         siteName + '-'
-                                        + channel.getID(  ).hashCode(  ) );
+                                        + channel.getID(  ).hashCode(  ),
+                                        outFile );
                                 }
                                 catch( IOException ex )
                                 {
@@ -106,6 +144,33 @@ public class GrabForWeb
                 }
             }
         }
+    }
+
+    protected static String getFNbyCN( String ChannelName )
+    {
+
+        char c;
+        String cz;
+        String result = "";
+
+        for( int i = 0; i < ChannelName.length(  ); i++ )
+        {
+            c = ChannelName.charAt( i );
+            cz = (String)CHARCONV.get( "" + c );
+
+            if( cz == null )
+            {
+                System.err.println( 
+                    "Warning: Error convert char to filename : '" + c
+                    + "' in string \"" + ChannelName + "\"" );
+            }
+            else
+            {
+                result += cz;
+            }
+        }
+
+        return result;
     }
 
     protected static class CmdProgress implements IProgress
