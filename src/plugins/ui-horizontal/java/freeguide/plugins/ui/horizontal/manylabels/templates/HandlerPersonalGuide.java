f44e36bf1a16209e6d10d9f826fab18aa68b2a92
alex73 <alex73@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2006-01-12 16:34:29 +0000

    sort programmes in personal guide
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@1237 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

--- src/plugins/ui-horizontal/java/freeguide/plugins/ui/horizontal/manylabels/templates/HandlerPersonalGuide.java
+++ src/plugins/ui-horizontal/java/freeguide/plugins/ui/horizontal/manylabels/templates/HandlerPersonalGuide.java
@@ -14,6 +14,7 @@ import java.text.DateFormat;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -312,6 +313,8 @@ public class HandlerPersonalGuide
                     }
                 }
             } );
+
+        Collections.sort( result );
 
         return result;
     }
