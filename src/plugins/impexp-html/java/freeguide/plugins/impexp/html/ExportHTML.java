b156dbfbb0476685808ecff9bdc233774c3392a5
alex73 <alex73@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2006-01-12 16:40:28 +0000

    export to html plugin
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@1239 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

---
+++ src/plugins/impexp-html/java/freeguide/plugins/impexp/html/ExportHTML.java
@@ -0,0 +1,214 @@
+package freeguide.plugins.impexp.html;
+
+import freeguide.gui.dialogs.FileChooserExtension;
+
+import freeguide.lib.fgspecific.data.TVData;
+
+import freeguide.lib.general.TemplateParser;
+
+import freeguide.plugins.BaseModule;
+import freeguide.plugins.IModuleExport;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import java.util.zip.GZIPOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileFilter;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author $author$
+ * @version $Revision$
+ */
+public class ExportHTML extends BaseModule implements IModuleExport
+{
+
+    protected final Config config = new Config(  );
+
+    /**
+     * DOCUMENT_ME!
+     *
+     * @param data DOCUMENT_ME!
+     * @param parent DOCUMENT_ME!
+     *
+     * @throws Exception DOCUMENT_ME!
+     */
+    public void exportData( TVData data, JFrame parent )
+        throws Exception
+    {
+
+        JFileChooser chooser = new JFileChooser(  );
+
+        if( config.path != null )
+        {
+            chooser.setSelectedFile( new File( config.path ) );
+        }
+
+        final FileFilter zipFilter =
+            new FileFilter(  )
+            {
+                public String getDescription(  )
+                {
+
+                    return "Zipped HTML file (*.zip)";
+                }
+
+                public boolean accept( File f )
+                {
+
+                    return !f.isDirectory(  )
+                    && f.getName(  ).endsWith( ".zip" );
+                }
+            };
+
+        final FileFilter gzipFilter =
+            new FileFilter(  )
+            {
+                public String getDescription(  )
+                {
+
+                    return "Gzipped HTML file (*.gz)";
+                }
+
+                public boolean accept( File f )
+                {
+
+                    return !f.isDirectory(  )
+                    && f.getName(  ).endsWith( ".gz" );
+                }
+            };
+
+        final FileFilter htmlFilter =
+            new FileFilter(  )
+            {
+                public String getDescription(  )
+                {
+
+                    return "HTML file (*.html, *.htm)";
+                }
+
+                public boolean accept( File f )
+                {
+
+                    return !f.isDirectory(  )
+                    && ( f.getName(  ).endsWith( ".htm" )
+                    || f.getName(  ).endsWith( ".html" ) );
+                }
+            };
+
+        chooser.addChoosableFileFilter( gzipFilter );
+        chooser.addChoosableFileFilter( zipFilter );
+        chooser.addChoosableFileFilter( htmlFilter );
+
+        chooser.setFileSelectionMode( JFileChooser.FILES_ONLY );
+        chooser.setMultiSelectionEnabled( false );
+
+        final FileChooserExtension ext = new FileChooserExtension(  );
+        chooser.setAccessory( ext );
+
+        if( chooser.showSaveDialog( parent ) == JFileChooser.APPROVE_OPTION )
+        {
+
+            String path = chooser.getSelectedFile(  ).getPath(  );
+            String lowerPath = path.toLowerCase(  );
+
+            if( 
+                ( chooser.getFileFilter(  ) == zipFilter )
+                    && !lowerPath.endsWith( ".zip" ) )
+            {
+                path += ".zip";
+            }
+
+            if( 
+                ( chooser.getFileFilter(  ) == gzipFilter )
+                    && !lowerPath.endsWith( ".gz" ) )
+            {
+                path += ".gz";
+            }
+
+            if( 
+                ( chooser.getFileFilter(  ) == htmlFilter )
+                    && !( lowerPath.endsWith( ".html" )
+                    || ( lowerPath.endsWith( ".htm" ) ) ) )
+            {
+                path += ".html";
+            }
+
+            config.path = path;
+            lowerPath = path.toLowerCase(  );
+
+            TemplateParser parser =
+                new TemplateParser( 
+                    ExportHTML.class.getPackage(  ).getName(  ).replace( 
+                        '.', '/' ) + "/template.html" );
+            final OutputStream outStream;
+
+            if( lowerPath.endsWith( ".zip" ) )
+            {
+
+                ZipOutputStream zipOut =
+                    new ZipOutputStream( new FileOutputStream( path ) );
+                ZipEntry zipEntry = new ZipEntry( "tv.html" );
+                zipOut.putNextEntry( zipEntry );
+                outStream = zipOut;
+            }
+            else if( lowerPath.endsWith( ".gz" ) )
+            {
+                outStream =
+                    new GZIPOutputStream( new FileOutputStream( path ) );
+            }
+            else
+            {
+                outStream = new FileOutputStream( path );
+            }
+
+            final Writer out =
+                new BufferedWriter( 
+                    new OutputStreamWriter( outStream, "UTF-8" ) );
+
+            try
+            {
+                parser.process( new TemplateHandler( ext ), out );
+            }
+            finally
+            {
+                out.flush(  );
+                out.close(  );
+            }
+        }
+    }
+
+    /**
+     * DOCUMENT_ME!
+     *
+     * @return DOCUMENT_ME!
+     */
+    public Object getConfig(  )
+    {
+
+        return config;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @author $author$
+     * @version $Revision$
+     */
+    public static class Config
+    {
+
+        /** Path of last saved file. */
+        public String path;
+    }
+}
---
+++ src/plugins/impexp-html/java/freeguide/plugins/impexp/html/TemplateHandler.java
@@ -0,0 +1,135 @@
+package freeguide.plugins.impexp.html;
+
+import freeguide.gui.dialogs.FileChooserExtension;
+
+import freeguide.lib.fgspecific.Application;
+import freeguide.lib.fgspecific.data.TVChannelsSet;
+import freeguide.lib.fgspecific.data.TVData;
+import freeguide.lib.fgspecific.data.TVProgramme;
+
+import freeguide.plugins.IModuleStorage;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author $author$
+ * @version $Revision$
+ */
+public class TemplateHandler
+{
+
+    protected final boolean selectedOnly;
+    protected final IModuleStorage.Info info;
+    protected final DateFormat timeFormat;
+
+    /**
+     * Creates a new TemplateHandler object.
+     *
+     * @param ext DOCUMENT ME!
+     */
+    public TemplateHandler( final FileChooserExtension ext )
+    {
+        selectedOnly = ext.isSelectedOnly(  );
+        info = ext.getSaveInfo(  );
+
+        timeFormat = new SimpleDateFormat( "dd MMMM yyyy, HH:mm" );
+        timeFormat.setTimeZone( Application.getInstance(  ).getTimeZone(  ) );
+    }
+
+    /**
+     * DOCUMENT_ME!
+     *
+     * @return DOCUMENT_ME!
+     */
+    public String getTitle(  )
+    {
+
+        return "listing";
+    }
+
+    /**
+     * DOCUMENT_ME!
+     *
+     * @return DOCUMENT_ME!
+     */
+    public String getHeader(  )
+    {
+
+        return "listing";
+    }
+
+    /**
+     * DOCUMENT_ME!
+     *
+     * @return DOCUMENT_ME!
+     */
+    public Collection getChannels(  )
+    {
+
+        return info.channelsList.getChannels(  );
+    }
+
+    /**
+     * DOCUMENT_ME!
+     *
+     * @param channel DOCUMENT_ME!
+     *
+     * @return DOCUMENT_ME!
+     *
+     * @throws Exception DOCUMENT_ME!
+     */
+    public Collection getProgrammes( final TVChannelsSet.Channel channel )
+        throws Exception
+    {
+
+        final IModuleStorage.Info filter = info.cloneInfo(  );
+        filter.channelsList.getChannels(  ).clear(  );
+        filter.channelsList.getChannels(  ).add( channel );
+
+        final TVData data =
+            Application.getInstance(  ).getDataStorage(  ).get( filter );
+        final Set programmes =
+            data.get( channel.getChannelID(  ) ).getProgrammes(  );
+
+        if( selectedOnly )
+        {
+
+            // remove non selected programmes from list
+            for( Iterator it = programmes.iterator(  ); it.hasNext(  ); )
+            {
+
+                final TVProgramme programme = (TVProgramme)it.next(  );
+
+                if( 
+                    !Application.getInstance(  ).getReminders(  )[0]
+                        .isSelected( programme ) )
+                {
+                    it.remove(  );
+                }
+            }
+        }
+
+        return programmes;
+    }
+
+    /**
+     * DOCUMENT_ME!
+     *
+     * @param programme DOCUMENT_ME!
+     *
+     * @return DOCUMENT_ME!
+     */
+    public String getProgrammeStartTime( final TVProgramme programme )
+    {
+
+        return timeFormat.format( new Date( programme.getStart(  ) ) );
+    }
+}
