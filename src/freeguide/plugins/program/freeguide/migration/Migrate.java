3f74ee09977036e7e99bc431c1d8fd8ee5cd7702
axis3x3 <axis3x3@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2006-04-30 07:43:58 +0000

    Added the required scaffolding for the migration from 0.10.3 to 0.10.4.
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@1295 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

--- src/freeguide/plugins/program/freeguide/migration/Migrate.java
+++ src/freeguide/plugins/program/freeguide/migration/Migrate.java
@@ -61,28 +61,43 @@ public class Migrate
 
                 migr = new Migrate0_10_2To0_10_3( migr.getResult(  ) );
                 migr.migrate(  );
-
+                
+                migr = new Migrate0_10_3To0_10_4( migr.getResult(  ) );
+                migr.migrate(  );
+                
                 migr.saveTo( FreeGuide.PREF_ROOT_NAME );
             }
             else if( ver.compareTo( new Version( 0, 10, 1 ) ) == 0 )
             {
-
                 MigrationProcessBase migr =
                     new Migrate0_10_1To0_10_2( FreeGuide.PREF_ROOT_NAME );
                 migr.migrate(  );
 
                 migr = new Migrate0_10_2To0_10_3( migr.getResult(  ) );
                 migr.migrate(  );
+                
+                migr = new Migrate0_10_3To0_10_4( migr.getResult(  ) );
+                migr.migrate(  );
 
                 migr.saveTo( FreeGuide.PREF_ROOT_NAME );
             }
             else if( ver.compareTo( new Version( 0, 10, 2 ) ) == 0 )
             {
-
                 MigrationProcessBase migr =
                     new Migrate0_10_2To0_10_3( FreeGuide.PREF_ROOT_NAME );
                 migr.migrate(  );
+                
+                migr = new Migrate0_10_3To0_10_4( migr.getResult(  ) );
+                migr.migrate(  );
 
+                migr.saveTo( FreeGuide.PREF_ROOT_NAME );
+            }
+            else if( ver.compareTo( new Version( 0, 10, 3 ) ) == 0 )
+            {
+                MigrationProcessBase migr =
+                    new Migrate0_10_3To0_10_4( FreeGuide.PREF_ROOT_NAME );
+                migr.migrate(  );
+                
                 migr.saveTo( FreeGuide.PREF_ROOT_NAME );
             }
             else if( ver.compareTo( Application.VERSION ) > 0 )
