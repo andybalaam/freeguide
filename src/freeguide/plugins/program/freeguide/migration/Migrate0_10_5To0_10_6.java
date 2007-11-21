7cc07af8a7bd8930039fa06f6dce764e57359202
axis3x3 <axis3x3@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2007-11-21 22:23:46 +0000

    Added a file missing from previous checkin - ensure that when upgrading to 0.10.6 the user's old XMLTV is deleted to make way for the new one.
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@1645 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

---
+++ src/freeguide/plugins/program/freeguide/migration/Migrate0_10_5To0_10_6.java
@@ -0,0 +1,67 @@
+package freeguide.plugins.program.freeguide.migration;
+
+import freeguide.plugins.program.freeguide.FreeGuide;
+import java.util.prefs.Preferences;
+import freeguide.common.lib.general.FileHelper;
+
+import java.io.File;
+import java.util.Map;
+import java.util.prefs.BackingStoreException;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author $author$
+ * @version $Revision$
+ */
+public class Migrate0_10_5To0_10_6 extends MigrationProcessBase
+{
+/**
+     * Creates a new Migrate0_10_5To0_10_6 object.
+     *
+     * @param source DOCUMENT ME!
+     */
+    public Migrate0_10_5To0_10_6( final Map source )
+    {
+        super( source );
+    }
+
+/**
+     * Creates a new Migrate0_10_5To0_10_6 object.
+     *
+     * @param nodeName DOCUMENT ME!
+     *
+     * @throws BackingStoreException DOCUMENT ME!
+     */
+    public Migrate0_10_5To0_10_6( final String nodeName )
+        throws BackingStoreException
+    {
+        super( nodeName );
+    }
+
+    /**
+     * DOCUMENT_ME!
+     *
+     * @throws Exception DOCUMENT_ME!
+     */
+    public void migrate(  ) throws Exception
+    {
+        FreeGuide.log.info( "Upgrading preferences 0.10.5 -> 0.10.6" );
+
+        boolean isWindows = System.getProperty( "os.name" ).startsWith(
+            "Windows" );
+        // Delete the installed XMLTV version so we can unzip a newer one
+        if( isWindows )
+        {
+            final File xmltvDir =
+                new File( FreeGuide.config.workingDirectory, "xmltv" );
+            
+            FileHelper.deleteDir( xmltvDir );
+        }
+        
+        moveNode( "" );
+
+        getAndRemoveKey( "version" );
+        putKey( "version", "0.10.6" );
+    }
+}
