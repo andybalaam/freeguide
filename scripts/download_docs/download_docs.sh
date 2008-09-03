86a36d38b6a157619c2ef48da2f2c8b543412739
axis3x3 <axis3x3@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2008-09-03 10:25:35 +0000

    Remove the old documentation and replace it with a snapshot of the new web site. Also added a script to create the snapshot.
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@1708 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

---
+++ scripts/download_docs/download_docs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Run from inside scripts/download_docs/
+
+cd ../../doc/
+
+rm -r www.artificialworlds.net
+
+wget -nv --mirror --no-parent --convert-links --html-extension http://www.artificialworlds.net/freeguide/
+
+cd www.artificialworlds.net/freeguide/
+find ./ -name "*action=*.html" -delete
+
+for RE in \
+	"<a [^>]*%3Faction=[^>]*>[^<]*<\/a>" \
+	"<a[^>]*AllRecentChanges[^>]*>All<\/a>" \
+	"<a[^>]*Site\/Search[^>]*>Search<\/a>" \
+	"<a accesskey=''  class='selflink' href='[^']*HomePage.html'>View<\/a>" \
+	; do
+{
+	find ./ -name "*.html" | xargs perl -pi -e "s/$RE//g;"
+}; done
+
+cd ../../
+rm -r html-local
+mv www.artificialworlds.net/freeguide html-local
+rmdir www.artificialworlds.net
+
