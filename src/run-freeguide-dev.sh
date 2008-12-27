ca5bcd62abec1dbe9c7a647a44abff0015f35cc4
axis3x3 <axis3x3@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2008-12-27 02:41:22 +0000

    Log all output to log.txt when we run.
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@1717 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

--- src/run-freeguide-dev.sh
+++ src/run-freeguide-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /home/andy/cvs/freeguide-tv/src/
-java freeguide.startup.Startup --pref_root /org/freeguide-tv-dev
+java freeguide.startup.Startup --pref_root /org/freeguide-tv-dev > log.txt 2>&1
 
