fd26cf8209a8fc0944afe18f6a96163f43359373
axis3x3 <axis3x3@0c3ae10b-d311-0410-bb6f-b905d6bbe80d>
2009-11-19 01:49:11 +0000

    Update the update-web script with the new way of doing it on sourceforge.
    git-svn-id: https://freeguide-tv.svn.sourceforge.net/svnroot/freeguide-tv/trunk/freeguide-tv@1770 0c3ae10b-d311-0410-bb6f-b905d6bbe80d

--- doc/sourceforge-redirects/upload-web.sh
+++ doc/sourceforge-redirects/upload-web.sh
@@ -2,5 +2,5 @@
 
 # Run this from inside the html-web dir.
 
-scp $@ axis3x3@shell.sourceforge.net:/home/groups/f/fr/freeguide-tv/htdocs/
+scp $@ axis3x3,freeguide-tv@web.sourceforge.net:/home/groups/f/fr/freeguide-tv/htdocs/
 
