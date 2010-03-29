d9c6e38a74443740751221dea140b63e3f1527d7
Andy Balaam <andybalaam@artificialworlds.net>
2010-03-29 23:49:11 +0100

    Implemented more (already passing) tests for the PathSearcher.

--- src/freeguide/test/fast/PathSearcherFastTest.java
+++ src/freeguide/test/fast/PathSearcherFastTest.java
@@ -44,8 +44,9 @@ public class PathSearcherFastTest
     throws Exception
     {
         test_FoundFirst();
-        //test_FoundLast();
-        //test_NotFound();
+        test_FoundLastExe();
+        test_FoundInFirstDir();
+        test_NotFound();
     }
 
     private void test_FoundFirst()
@@ -60,6 +61,48 @@ public class PathSearcherFastTest
         String ans = ps.findInPath( exeNames, "fallback" );
 
         FreeGuideTest.my_assert( ans.equals( "exe1" ) );
+    }
+
+    private void test_FoundLastExe()
+    throws Exception
+    {
+        String[] dirsToSearch = { "/dir/dir1", "/dir2" };
+        String[] exesThatExist = { "/dir2/exe2" };
+        FakePathSearcher ps = new FakePathSearcher( dirsToSearch,
+            exesThatExist );
+
+        String[] exeNames = { "exe1", "exe2" };
+        String ans = ps.findInPath( exeNames, "fallback" );
+
+        FreeGuideTest.my_assert( ans.equals( "exe2" ) );
+    }
+
+    private void test_FoundInFirstDir()
+    throws Exception
+    {
+        String[] dirsToSearch = { "/dir/dir1", "/dir2" };
+        String[] exesThatExist = { "/dir/dir1/exe2", "/dir2/exe1" };
+        FakePathSearcher ps = new FakePathSearcher( dirsToSearch,
+            exesThatExist );
+
+        String[] exeNames = { "exe1", "exe2" };
+        String ans = ps.findInPath( exeNames, "fallback" );
+
+        FreeGuideTest.my_assert( ans.equals( "exe2" ) );
+    }
+
+    private void test_NotFound()
+    throws Exception
+    {
+        String[] dirsToSearch = { "/dir/dir1", "/dir2" };
+        String[] exesThatExist = { "/dir/dir1/exe7", "/dir2/exe8" };
+        FakePathSearcher ps = new FakePathSearcher( dirsToSearch,
+            exesThatExist );
+
+        String[] exeNames = { "exe1", "exe2" };
+        String ans = ps.findInPath( exeNames, "fallback" );
+
+        FreeGuideTest.my_assert( ans.equals( "fallback" ) );
     }
 
 }
