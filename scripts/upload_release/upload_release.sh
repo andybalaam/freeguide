147b3f9211e8656d8d3a6d31fa58e19e3288898c
Andy Balaam <andybalaam@artificialworlds.net>
2010-06-21 00:46:28 +0100

    Added mkdirs to the release upload script.

--- scripts/upload_release/upload_release.sh
+++ scripts/upload_release/upload_release.sh
@@ -5,7 +5,9 @@
 # TODO: get version number from file names
 VERSION=0.11
 
+echo mkdir /home/frs/project/f/fr/freeguide-tv/freeguide/$VERSION/ | sftp -b - axis3x3,freeguide-tv@frs.sourceforge.net
 scp dist/bin/* axis3x3,freeguide-tv@frs.sourceforge.net:/home/frs/project/f/fr/freeguide-tv/freeguide/$VERSION/
 
+echo mkdir /home/frs/project/f/fr/freeguide-tv/freeguide-source/$VERSION/ | sftp -b - axis3x3,freeguide-tv@frs.sourceforge.net
 scp dist/src/* axis3x3,freeguide-tv@frs.sourceforge.net:/home/frs/project/f/fr/freeguide-tv/freeguide-source/$VERSION/
 
